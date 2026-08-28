b078bdb3c4a73f066ac0bc9ff6ed78838bf818a2
zaryabmalik314-code <zaryabmalik314@gmail.com>
2026-08-29 01:23:17 +0500

    Change server listen address to '0.0.0.0'

--- cnn-live-viz/server.js
+++ cnn-live-viz/server.js
@@ -59,4 +59,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
